1844433d794b193d51a824ea523e51b9dabac4f0
skimeMa <747391119@qq.com>
2023-02-10 16:10:24 +0800

    feat: change login page ui

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,28 +1,28 @@
-import { Form, Input, Button } from 'antd'
-import { useNavigate } from 'react-router'
-import { LockOutlined, UserOutlined } from '@ant-design/icons'
-import { useRequest } from 'ahooks'
-import { login } from '@/services/auth/auth.service'
-import useTitle from 'ahooks/lib/useTitle'
-import { APP_NAME } from '@/utils/constants'
-import { LoginBox, PageMain, LoginTitle } from './style'
-import { LoginRes } from '@/services/auth/auth.model'
+import { Form, Input, Button } from "antd"
+import { useNavigate } from "react-router"
+import { LockOutlined, UserOutlined } from "@ant-design/icons"
+import { useRequest } from "ahooks"
+import { login } from "@/services/auth/auth.service"
+import useTitle from "ahooks/lib/useTitle"
+import { APP_NAME } from "@/utils/constants"
+import { LoginBox, PageMain, LoginTitle } from "./style"
+import { LoginRes } from "@/services/auth/auth.model"
 
 const LoginPage = () => {
-	useTitle(`${APP_NAME}-密码登录`)
+	useTitle(`${APP_NAME}-用户登录`)
 	const navigate = useNavigate()
 	const { loading, run } = useRequest(login, {
 		manual: true,
 		onSuccess: (res: LoginRes) => {
-			window.sessionStorage.setItem('mtsToken', res.token || '')
-			navigate('/')
+			window.sessionStorage.setItem("mtsToken", res.token || "")
+			navigate("/")
 		}
 	})
 
 	return (
 		<PageMain>
 			<LoginBox>
-				<LoginTitle>用户登录</LoginTitle>
+				<LoginTitle>{APP_NAME}</LoginTitle>
 				<Form
 					layout='vertical'
 					onFinish={values => {
@@ -35,7 +35,13 @@ const LoginPage = () => {
 						<Input size='large' type='text' prefix={<UserOutlined />} placeholder='请输入用户名' />
 					</Form.Item>
 					<Form.Item required label='密码' name='password'>
-						<Input size='large' autoComplete='new-password' type='password' prefix={<LockOutlined />} placeholder='请输入密码' />
+						<Input
+							size='large'
+							autoComplete='new-password'
+							type='password'
+							prefix={<LockOutlined />}
+							placeholder='请输入密码'
+						/>
 					</Form.Item>
 					<Form.Item>
 						<Button
@@ -43,7 +49,7 @@ const LoginPage = () => {
 							htmlType='submit'
 							type='primary'
 							size='large'
-							style={{ width: '100%', marginBottom: '5px' }}
+							style={{ width: "100%", marginBottom: "5px" }}
 						>
 							登录
 						</Button>
--- src/pages/Login/style.ts
+++ src/pages/Login/style.ts
@@ -1,15 +1,17 @@
-import styled from '@emotion/styled'
+import styled from "@emotion/styled"
 
 export const PageMain = styled.div`
 	width: 100vw;
 	height: 100vh;
-	background-color: #f0f2f5;
+	background-color: #1677ff57;
 `
 
 export const LoginTitle = styled.div`
 	text-align: center;
 	font-size: 28px;
 	margin-bottom: 40px;
+	color: #1677ff;
+	font-weight: 700;
 `
 
 export const LoginBox = styled.div`
@@ -21,7 +23,7 @@ export const LoginBox = styled.div`
 	width: 400px;
 	height: 300px;
 	background-color: #fff;
-	box-shadow: 0 1px 3px 0 rgb(0 0 0 / 4%);
 	border: 1px solid #eaedf3;
-	border-radius: 4px;
+	border-radius: 10px;
+	box-shadow: 0 0.9rem 1.7rem rgba(0, 0, 0, 0.25), 0 0.7rem 0.7rem rgba(0, 0, 0, 0.22);
 `
