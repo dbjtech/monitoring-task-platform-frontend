351c5a855a55df8d3e2f433c1ce357a4af63ebc5
skimeMa <747391119@qq.com>
2023-02-24 09:54:16 +0800

    refactor: use ahook useSessionStorageState

--- src/components/HeaderActions/index.tsx
+++ src/components/HeaderActions/index.tsx
@@ -1,22 +1,27 @@
-import { logout as logoutService } from '@/services/auth/auth.service'
-import { useNavigate } from 'react-router-dom'
-import { Logout } from './style'
-import { useCallback } from 'react'
-import { Modal } from 'antd'
-import { ExclamationCircleOutlined } from '@ant-design/icons'
+import { logout as logoutService } from "@/services/auth/auth.service"
+import { useNavigate } from "react-router-dom"
+import { Logout } from "./style"
+import { useCallback } from "react"
+import { Modal } from "antd"
+import { ExclamationCircleOutlined } from "@ant-design/icons"
+import { useSessionStorageState } from "ahooks"
 
 const HeaderActions = () => {
 	const navigate = useNavigate()
+	const [tokenStorage, setTokenStorage] = useSessionStorageState("mtsToken", {
+		serializer: (value: any) => value,
+		deserializer: (value: any) => value
+	})
 
 	const showLogoutConfirm = useCallback(() => {
 		Modal.confirm({
-			title: '退出登录',
+			title: "退出登录",
 			icon: <ExclamationCircleOutlined />,
-			content: '确认退出？',
+			content: "确认退出？",
 			onOk() {
 				logoutService().then(() => {
-					window.sessionStorage.removeItem('mtsToken')
-					navigate('/entrance/login')
+					setTokenStorage(undefined)
+					navigate("/entrance/login")
 				})
 			}
 		})
